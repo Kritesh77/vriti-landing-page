6fbe3a98f79f74a39cdc5f6f096b7ac8cc4524a1
Kritesh Kaushik <kritesh@orgzit.com>
2023-04-08 13:30:44 +0530

    minor overlay fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.onload = (event) => {
   var disclaimerContainer = document.getElementById("disclaimer-container");
   let isDisclaimerAgreed = localStorage.getItem("disclaimerAgreed");
+  let body = document.getElementById("body");
+  body.style.overflow = "hidden";
   disclaimerContainer.style.display = "flex";
   // if (isDisclaimerAgreed) {
   //   disclaimerContainer.style.display = "none";
@@ -12,4 +14,6 @@ function handleDisclaimerClick() {
   var disclaimerContainer = document.getElementById("disclaimer-container");
   localStorage.setItem("disclaimerAgreed", true);
   disclaimerContainer.style.display = "none";
+  let body = document.getElementById("body");
+  body.style.overflow = "auto";
 }
